652491aae4a1b8092ca8115e28fbd5b1924c1b5f
Andrew Johnson <andrew.johnson@handsome.is>
2018-12-24 10:00:50 -0600

    Add call to fetch Spotify tokens

--- app/src/main/java/com/meaningless/powerhour/data/services/SpotifyAccountAPI.kt
+++ app/src/main/java/com/meaningless/powerhour/data/services/SpotifyAccountAPI.kt
@@ -3,7 +3,7 @@ package com.meaningless.powerhour.data.services
 import com.meaningless.powerhour.BuildConfig
 import com.meaningless.powerhour.data.models.SpotifyLoginResponse
 import retrofit2.Call
-import retrofit2.http.GET
+import retrofit2.http.*
 
 interface SpotifyAccountAPI {
 
@@ -16,5 +16,15 @@ interface SpotifyAccountAPI {
         &scope=${BuildConfig.SPOTIFY_SCOPES}
         """
     )
-    fun login(): Call<SpotifyLoginResponse>
+    fun requestAuthorization(): Call<SpotifyLoginResponse>
+
+    @Headers("Authorization: Basic ${BuildConfig.SPOTIFY_AUTH_TOKEN}")
+    @POST(
+        """
+        api/token
+        ?grant_type=authorization_code
+        &redirect_uri=${BuildConfig.SPOTIFY_REDIRECT_URI}
+        """
+    )
+    fun requestTokens(@Query("code") code: String): Call<SpotifyLoginResponse>
 }
